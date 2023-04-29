127550e25aac19360c75ee83c08881db09d9dfe7
rmens <r.mens@me.com>
2023-04-29 23:22:20 +0200

    Formatting and consistency

--- pisetup.sh
+++ pisetup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Function that checks if this a supported platform
+# Function that checks if this is a supported platform
 function check_platform() {
-  if ! grep "Raspberry Pi 4" /proc/device-tree/model &> /dev/null; then
+  if ! grep "Raspberry Pi 4" /proc/device-tree/model &>/dev/null; then
     echo -e "\e[1;31;5m** NOT RUNNING ON A RASPBERRY PI 4 **\e[0m"
     read -rp $'\e[3m\e[33mThis script is only tested on a Raspberry Pi 4. Press Enter to continue anyway...\e[0m\n'
   fi
@@ -19,19 +19,19 @@ check_platform
 
 # Check if the micrompx service is running
 if systemctl is-active --quiet micrompx; then
-    echo "MicroMPX service is running. Stopping it now..."
-    
-    # Stop the micrompx service
-    systemctl stop micrompx
-    
-    if [ $? -eq 0 ]; then
-        echo "MicroMPX service stopped successfully. We can now upgrade of reinstall."
-    else
-        echo "Failed to stop the MicroMPX service. Please check the logs for more details."
-		exit 1
-    fi
+  echo "MicroMPX service is running. Stopping it now..."
+
+  # Stop the micrompx service
+  systemctl stop micrompx
+
+  if [ $? -eq 0 ]; then
+    echo "MicroMPX service stopped successfully. We can now upgrade or reinstall."
+  else
+    echo "Failed to stop the MicroMPX service. Please check the logs for more details."
+    exit 1
+  fi
 else
-    echo "MicroMPX service is not running. Ussuming fresh install."
+  echo "MicroMPX service is not running. Assuming fresh install."
 fi
 
 # Expand the filesystem
@@ -47,15 +47,12 @@ apt -qq -y upgrade >/dev/null 2>&1
 apt -qq -y autoremove >/dev/null 2>&1
 
 # Add the user micrompx if it doesn't exist
-if ! id -u micrompx > /dev/null 2>&1; then 
+if ! id -u micrompx > /dev/null 2>&1; then
   useradd -m micrompx --home /home/micrompx --shell /usr/sbin/nologin --comment "micrompx daemon user"
 fi
 
 # Install dependencies
-apt -qq -y install libasound2 
-
-# Create working dir if it doesn't exist
-mkdir -p /opt/micrompx
+apt -qq -y install libasound2
 
 # Download MicroMPX 64bit and make it executable
 wget https://download.thimeo.com/MicroMPX_Decoder_ARM64 -O /opt/micrompx/MicroMPX_Decoder
