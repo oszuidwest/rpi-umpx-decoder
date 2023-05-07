ca969374090c4a46b5f5471f5e646bf454d3eb5f
rmens <r.mens@me.com>
2023-05-07 13:19:50 +0200

    Merge and refactor two scripts

--- pisetup.sh
+++ pisetup.sh
@@ -59,3 +59,46 @@ wget https://raw.githubusercontent.com/oszuidwest/rpi-umpx-decoder/main/micrompx
 systemctl daemon-reload
 systemctl enable micrompx
 service micrompx restart
+
+echo "Disabling onboard audio..."
+CONFIG_FILE="/boot/config.txt"
+sed -i '/^[^#]*dtparam=audio=on/s/^/#/' $CONFIG_FILE
+sed -i 's/dtoverlay=vc4-fkms-v3d/dtoverlay=vc4-fkms-v3d,audio=off/g' $CONFIG_FILE
+sed -i 's/dtoverlay=vc4-kms-v3d/dtoverlay=vc4-kms-v3d,noaudio/g' $CONFIG_FILE
+
+echo "Configuring device tree overlay for HifiBerry..."
+echo "Enter the number corresponding to your device:"
+echo "1. DAC FOR RASPBERRY PI 1/DAC+ LIGHT/DAC ZERO/MINIAMP/BEOCREATE/DAC+ DSP/DAC+ RTC"
+echo "2. DAC+ STANDARD/PRO/AMP2"
+echo "3. DAC2 HD"
+echo "4. DAC+ ADC"
+echo "5. DAC+ ADC PRO"
+echo "6. DIGI+"
+echo "7. DIGI+ PRO"
+
+read -r device_number
+case $device_number in
+    1) overlay="hifiberry-dac" ;;
+    2) overlay="hifiberry-dacplus" ;;
+    3) overlay="hifiberry-dacplushd" ;;
+    4) overlay="hifiberry-dacplusadc" ;;
+    5) overlay="hifiberry-dacplusadcpro" ;;
+    6) overlay="hifiberry-digi" ;;
+    7) overlay="hifiberry-digi-pro" ;;
+    *) echo "Invalid input, exiting."; exit 1 ;;
+esac
+
+if ! grep -q "dtoverlay=$overlay" $CONFIG_FILE; then
+    echo "dtoverlay=$overlay" >> $CONFIG_FILE
+fi
+
+echo "Checking Linux version and disabling onboard EEPROM if necessary..."
+kernel_version=$(uname -r | awk -F. '{print $1 "." $2}')
+
+if [ "$(printf "%s\\n" "5.4" "$kernel_version" | sort -V | head -n1)" = "5.4" ] && [ "$kernel_version" != "5.4" ]; then
+    if ! grep -q 'force_eeprom_read=0' $CONFIG_FILE; then
+        echo "force_eeprom_read=0" >> $CONFIG_FILE
+    fi
+fi
+
+echo "Configuration updated. Please reboot your system for the changes to take effect."
