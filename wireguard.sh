29786024228b013c2a3cf5a6ba999bedc51e7426
rmens <r.mens@me.com>
2023-05-08 23:57:20 +0200

    Create WG install script

---
+++ wireguard.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Variables (Replace these with the actual values)
+server_ip="<server_ip>"
+server_public_key="<server_public_key>"
+raspberry_address="172.16.0.2/24"
+raspberry_private_key="<raspberry_private_key>"
+
+# Update and install WireGuard
+echo "Updating system and installing WireGuard..."
+apt update
+apt install -y wireguard
+
+# Generate key pair if it doesn't exist
+umask 077
+mkdir -p /etc/wireguard
+cd /etc/wireguard
+
+if [ ! -f privatekey ] || [ ! -f publickey ]; then
+  echo "Generating new key pair..."
+  wg genkey | tee privatekey | wg pubkey | tee publickey
+  echo "New key pair has been generated and saved in /etc/wireguard."
+else
+  echo "Key pair already exists in /etc/wireguard. No new keys generated."
+fi
+
+# Create WireGuard configuration file
+echo "Creating WireGuard configuration file..."
+bash -c "cat > /etc/wireguard/wg0.conf << EOL
+[Interface]
+Address = ${raspberry_address}
+PrivateKey = ${raspberry_private_key}
+
+[Peer]
+PublicKey = ${server_public_key}
+Endpoint = ${server_ip}:51820
+AllowedIPs = 172.16.0.0/24
+PersistentKeepalive = 25
+EOL"
+
+# Enable and start WireGuard
+echo "Enabling and starting WireGuard..."
+systemctl enable wg-quick@wg0
+systemctl start wg-quick@wg0
+
+# Check connectivity
+echo "Checking connectivity to the server..."
+ping -c 3 ${server_ip}
+
+echo "WireGuard VPN configuration completed!"
