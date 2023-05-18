73b9122c9257cd9cc66f41abeb0d1ed704c6b4b1
rmens <r.mens@me.com>
2023-05-18 15:22:08 +0200

    Update pisetup.sh

--- pisetup.sh
+++ pisetup.sh
@@ -8,20 +8,20 @@ readonly BLUE='\033[1;34m'
 readonly NC='\033[0m' # No Color
 
 # Hi!
-printf "${GREEN}⎎ MicroMPX Setup for Raspberry Pi 4${NC}\n\n"
+echo -e "${GREEN}⎎ MicroMPX Setup for Raspberry Pi 4${NC}\n\n"
 
 # Function that checks if this is a supported platform
 check_platform() {
   if ! grep -q "Raspberry Pi 4" /proc/device-tree/model > /dev/null; then
-    printf "${RED}** NOT RUNNING ON A RASPBERRY PI 4 **${NC}\n"
-    printf "${YELLOW}This script is only tested on a Raspberry Pi 4. Press Enter to continue anyway...${NC}\n"
-	read -r
+    echo -e "${RED}** NOT RUNNING ON A RASPBERRY PI 4 **${NC}"
+    echo -e "${YELLOW}This script is only tested on a Raspberry Pi 4. Press Enter to continue anyway...${NC}"
+    read -r
   fi
 }
 
 # Check if running as root
 if [[ "$(id -u)" -ne 0 ]]; then
-  printf "${RED}This script must be run as root. Please run 'sudo su' first.${NC}\n"
+  echo -e "${RED}This script must be run as root. Please run 'sudo su' first.${NC}"
   exit 1
 fi
 
@@ -29,76 +29,76 @@ fi
 check_platform
 
 # Check and stop micrompx service if running
-printf "${BLUE}►► Checking and stopping MicroMPX service if running...${NC}\n\n"
+echo -e "${BLUE}►► Checking and stopping MicroMPX service if running...${NC}"
 if systemctl is-active --quiet micrompx > /dev/null; then
-  systemctl stop micrompx > /dev/null || { printf "${RED}Failed to stop the MicroMPX service. Please check the logs for more details.${NC}\n"; exit 1; }
+  systemctl stop micrompx > /dev/null || { echo -e "${RED}Failed to stop the MicroMPX service. Please check the logs for more details.${NC}"; exit 1; }
 else
-  printf "${YELLOW}MicroMPX service is not running. This it either a fresh install or the stop succeeded.${NC}\n"
+  echo -e "${YELLOW}MicroMPX service is not running. This it either a fresh install or the stop succeeded.${NC}"
 fi
 
 # Expand filesystem
-printf "${BLUE}►► Expanding filesystem...${NC}\n\n"
+echo -e "${BLUE}►► Expanding filesystem...${NC}"
 raspi-config --expand-rootfs > /dev/null
 
 # Timezone configuration
-printf "${BLUE}►► Setting timezone to Europe/Amsterdam...${NC}\n\n"
+echo -e "${BLUE}►► Setting timezone to Europe/Amsterdam...${NC}"
 ln -fs /usr/share/zoneinfo/Europe/Amsterdam /etc/localtime > /dev/null
 dpkg-reconfigure -f noninteractive tzdata > /dev/null
 
 # Update the OS
-printf "${BLUE}►► Updating all packages...${NC}\n\n"
+echo -e "${BLUE}►► Updating all packages...${NC}"
 apt -qq -y update > /dev/null 2>&1
 apt -qq -y full-upgrade > /dev/null 2>&1
 apt -qq -y autoremove > /dev/null 2>&1
 
 # Add user for micrompx
-printf "${BLUE}►► Adding micrompx user if it doesn't exist...${NC}\n\n"
+echo -e "${BLUE}►► Adding micrompx user if it doesn't exist...${NC}"
 if ! id -u micrompx > /dev/null; then
   useradd -m micrompx --home /home/micrompx --shell /usr/sbin/nologin --comment "micrompx daemon user" > /dev/null
 fi
 
-printf "${BLUE}Checking if the user 'micrompx' is a member of the 'audio' group${NC}\n"
+echo -e "${BLUE}►► Checking if the user 'micrompx' is a member of the 'audio' group${NC}"
 if groups micrompx | grep -q '\baudio\b' > /dev/null; then
-    printf "${YELLOW}User 'micrompx' is already a member of the 'audio' group. Not doing anything.${NC}\n"
+    echo -e "${YELLOW}User 'micrompx' is already a member of the 'audio' group. Not doing anything.${NC}"
 else
-    printf "${YELLOW}User 'micrompx' is not a member of the 'audio' group. Adding them to the group now...${NC}\n"
+    echo -e "${YELLOW}User 'micrompx' is not a member of the 'audio' group. Adding them to the group now...${NC}"
     usermod -aG audio micrompx > /dev/null
 fi
 
 # Install dependencies for micrompx
-printf "${BLUE}►► Installing dependencies...${NC}\n\n"
+echo -e "${BLUE}►► Installing dependencies...${NC}"
 apt -qq -y install libasound2 > /dev/null 2>&1
 
 # Download micrompx for Thimeo
-printf "${BLUE}►► Downloading and installing MicroMPX...${NC}\n\n"
+echo -e "${BLUE}►► Downloading and installing MicroMPX...${NC}"
 mkdir -p /opt/micrompx > /dev/null
 wget -q https://www.stereotool.com/download/MicroMPX_Decoder_ARM64 -O /opt/micrompx/MicroMPX_Decoder > /dev/null
 chmod +x /opt/micrompx/MicroMPX_Decoder > /dev/null
 setcap CAP_NET_BIND_SERVICE=+eip /opt/micrompx/MicroMPX_Decoder > /dev/null
 
 # Add service
-printf "${BLUE}►► Installing MicroMPX service...${NC}\n\n"
+echo -e "${BLUE}►► Installing MicroMPX service...${NC}"
 rm -f /etc/systemd/system/micrompx.service > /dev/null
 wget -q https://raw.githubusercontent.com/oszuidwest/rpi-umpx-decoder/main/micrompx.service -O /etc/systemd/system/micrompx.service > /dev/null
 systemctl daemon-reload > /dev/null
 systemctl enable micrompx > /dev/null
 
 # Disable only the hdmi audio so we can use the minijack for monitoring
-printf "${BLUE}►► Disabling onboard audio...${NC}\n\n"
+echo -e "${BLUE}►► Disabling onboard audio...${NC}"
 readonly CONFIG_FILE="/boot/config.txt"
 sed -i '/dtoverlay=vc4-fkms-v3d/ { /audio=off/! s/$/,audio=off/ }' "$CONFIG_FILE" > /dev/null
 sed -i '/dtoverlay=vc4-kms-v3d/ { /noaudio/! s/$/,noaudio/ }' "$CONFIG_FILE" > /dev/null
 
 # Configure the HifiBerry
-printf "${BLUE}►► Configuring device tree overlay for HifiBerry...${NC}\n\n"
-printf "Enter the number corresponding to your device:\n"
-printf "1. DAC FOR RASPBERRY PI 1/DAC+ LIGHT/DAC ZERO/MINIAMP/BEOCREATE/DAC+ DSP/DAC+ RTC\n"
-printf "2. DAC+ STANDARD/PRO/XLR\n"
-printf "3. DAC2 HD\n"
-printf "4. DAC+ ADC\n"
-printf "5. DAC+ ADC PRO\n"
-printf "6. DIGI+\n"
-printf "7. DIGI+ PRO\n"
+echo -e "${BLUE}►► Configuring device tree overlay for HifiBerry...${NC}"
+echo -e "Enter the number corresponding to your device:"
+echo -e "1. DAC FOR RASPBERRY PI 1/DAC+ LIGHT/DAC ZERO/MINIAMP/BEOCREATE/DAC+ DSP/DAC+ RTC"
+echo -e "2. DAC+ STANDARD/PRO/XLR"
+echo -e "3. DAC2 HD"
+echo -e "4. DAC+ ADC"
+echo -e "5. DAC+ ADC PRO"
+echo -e "6. DIGI+"
+echo -e "7. DIGI+ PRO"
 
 read -r device_number
 case $device_number in
@@ -109,22 +109,21 @@ case $device_number in
     5) overlay="hifiberry-dacplusadcpro" ;;
     6) overlay="hifiberry-digi" ;;
     7) overlay="hifiberry-digi-pro" ;;
-    *) printf "${RED}Invalid input, exiting.${NC}\n"; exit 1 ;;
+    *) echo -e "${RED}Invalid input, exiting.${NC}"; exit 1 ;;
 esac
 
 if ! grep -q "dtoverlay=$overlay" $CONFIG_FILE > /dev/null; then
-    printf "dtoverlay=$overlay" >> $CONFIG_FILE
+    echo -e "dtoverlay=$overlay" >> $CONFIG_FILE
 fi
 
 # Apply HifiBerry kernel fix is needed
-printf "${BLUE}►► Checking Linux version and disabling onboard EEPROM if necessary...${NC}\n\n"
+echo -e "${BLUE}►► Checking Linux version and disabling onboard EEPROM if necessary...${NC}"
 kernel_version=$(uname -r | awk -F. '{print $1 "." $2}')
-
-if [ "$(printf "%s\\n" "5.4" "$kernel_version" | sort -V | head -n1)" = "5.4" ] && [ "$kernel_version" != "5.4" ]; then
-    if ! grep -q 'force_eeprom_read=0' $CONFIG_FILE > /dev/null; then
-        printf "force_eeprom_read=0" >> $CONFIG_FILE
-    fi
+if [ "$(printf '%s\n' "5.4" "$kernel_version" | sort -V | head -n1)" = "5.4" ] && [ "$kernel_version" != "5.4" ]; then
+    grep -q 'force_eeprom_read=0' $CONFIG_FILE || echo -e "force_eeprom_read=0" >> $CONFIG_FILE
 fi
 
-# Fin!
-printf "${GREEN}✓ Configuration updated. Please reboot your system for the changes to take effect.${NC}\n"
+# Reboot
+echo -e "\n\n${GREEN}✓ Setup is complete! Your Raspberry Pi will reboot in 10 seconds.${NC}"
+sleep 10
+reboot
