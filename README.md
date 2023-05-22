b1c0a75b8c7b9113116869a94a3e2647e025842a
Raymon Mens <r.mens@me.com>
2023-05-22 23:00:03 +0200

    Refresh VPN branch (#2)

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # rpi-umpx-decoder
-Raspberry Pi MicroMPX decoder (work-in-progress)
+This repository contains the MicroMPX set-up for [ZuidWest FM](https://www.zuidwestfm.nl/) in the Netherlands. It uses a Rapsberry Pi 4 and a [HiFiBerry DAC2 Pro XLR
+](https://www.hifiberry.com/shop/boards/hifiberry-dac2-pro-xlr/) as audio output. It downloads the most recent version of the MicroMPX decoder from Thimeo, which is managed by systemd as service.
 
-Download and run the install script with the command```/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/oszuidwest/rpi-umpx-decoder/main/pisetup.sh)"```
+# How to prepare the Rapsberry Pi
+- Install Raspberry Pi OS Lite 11 (bullseye) 64-bit
+- Ensure you are root by running `sudo su`
+- Download and run the install script with the command `/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/oszuidwest/rpi-umpx-decoder/main/pisetup.sh)"`
+
+# How to configure MicroMPX
+- If you want monitoring via UptimeRobot, add the contents of `allowlist.txt` to the MicroMPX configuration file
+
+[WIP]
--- pisetup.sh
+++ pisetup.sh
@@ -1,18 +1,38 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo -e "\e[1;32mMicroMPX Setup for Raspberry Pi 4\e[0m"
+# Set some colors
+readonly GREEN='\033[1;32m'
+readonly RED='\033[1;31m'
+readonly YELLOW='\033[0;33m'
+readonly BLUE='\033[1;34m'
+readonly NC='\033[0m' # No Color
+
+# Something fancy for the sysadmin
+clear
+cat << "EOF"
+ ______     _     ___          __       _     ______ __  __ 
+|___  /    (_)   | \ \        / /      | |   |  ____|  \/  |
+   / /_   _ _  __| |\ \  /\  / /__  ___| |_  | |__  | \  / |
+  / /| | | | |/ _` | \ \/  \/ / _ \/ __| __| |  __| | |\/| |
+ / /_| |_| | | (_| |  \  /\  /  __/\__ \ |_  | |    | |  | |
+/_____\__,_|_|\__,_|   \/  \/ \___||___/\__| |_|    |_|  |_|
+EOF
+
+# Hi!
+echo -e "${GREEN}⎎ MicroMPX Setup for Raspberry Pi 4${NC}\n\n"
 
 # Function that checks if this is a supported platform
-function check_platform() {
-  if ! grep "Raspberry Pi 4" /proc/device-tree/model &>/dev/null; then
-    echo -e "\e[1;31;5m** NOT RUNNING ON A RASPBERRY PI 4 **\e[0m"
-    read -rp $'\e[3m\e[33mThis script is only tested on a Raspberry Pi 4. Press Enter to continue anyway...\e[0m\n'
+check_platform() {
+  if ! grep -q "Raspberry Pi 4" /proc/device-tree/model > /dev/null; then
+    echo -e "${RED}** NOT RUNNING ON A RASPBERRY PI 4 **${NC}"
+    echo -e "${YELLOW}This script is only tested on a Raspberry Pi 4. Press Enter to continue anyway...${NC}"
+    read -r
   fi
 }
 
 # Check if running as root
-if [ "$(id -u)" -ne 0 ]; then
-  echo "This script must be run as root. Please use 'sudo bash pisetup.sh' to run it."
+if [[ "$(id -u)" -ne 0 ]]; then
+  echo -e "${RED}This script must be run as root. Please run 'sudo su' first.${NC}"
   exit 1
 fi
 
@@ -20,61 +40,76 @@ fi
 check_platform
 
 # Check and stop micrompx service if running
-echo "Checking and stopping MicroMPX service if running..."
-if systemctl is-active --quiet micrompx; then
-  systemctl stop micrompx || { echo "Failed to stop the MicroMPX service. Please check the logs for more details."; exit 1; }
+echo -e "${BLUE}►► Checking and stopping MicroMPX service if running...${NC}"
+if systemctl is-active --quiet micrompx > /dev/null; then
+  systemctl stop micrompx > /dev/null || { echo -e "${RED}Failed to stop the MicroMPX service. Please check the logs for more details.${NC}"; exit 1; }
 else
-  echo "MicroMPX service is not running. Assuming fresh install."
+  echo -e "${YELLOW}MicroMPX service is not running. This it either a fresh install or the stop succeeded.${NC}"
 fi
 
-echo "Expanding filesystem..."
-raspi-config --expand-rootfs
+# Expand filesystem
+echo -e "${BLUE}►► Expanding filesystem...${NC}"
+raspi-config --expand-rootfs > /dev/null
 
-echo "Setting timezone to Europe/Amsterdam..."
-ln -fs /usr/share/zoneinfo/Europe/Amsterdam /etc/localtime
-dpkg-reconfigure -f noninteractive tzdata
+# Timezone configuration
+echo -e "${BLUE}►► Setting timezone to Europe/Amsterdam...${NC}"
+ln -fs /usr/share/zoneinfo/Europe/Amsterdam /etc/localtime > /dev/null
+dpkg-reconfigure -f noninteractive tzdata > /dev/null
 
-echo "Updating all packages..."
-apt -qq -y update
-apt -qq -y upgrade
-apt -qq -y autoremove
+# Update the OS
+echo -e "${BLUE}►► Updating all packages...${NC}"
+apt -qq -y update > /dev/null 2>&1
+apt -qq -y full-upgrade > /dev/null 2>&1
+apt -qq -y autoremove > /dev/null 2>&1
 
-echo "Adding micrompx user if it doesn't exist..."
-if ! id -u micrompx > /dev/null 2>&1; then
-  useradd -m micrompx --home /home/micrompx --shell /usr/sbin/nologin --comment "micrompx daemon user"
+# Add user for micrompx
+echo -e "${BLUE}►► Adding micrompx user if it doesn't exist...${NC}"
+if ! id -u micrompx > /dev/null; then
+  useradd -m micrompx --home /home/micrompx --shell /usr/sbin/nologin --comment "micrompx daemon user" > /dev/null
 fi
 
-echo "Installing dependencies..."
-apt -qq -y install libasound2
+echo -e "${BLUE}►► Checking if the user 'micrompx' is a member of the 'audio' group${NC}"
+if groups micrompx | grep -q '\baudio\b' > /dev/null; then
+    echo -e "${YELLOW}User 'micrompx' is already a member of the 'audio' group. Not doing anything.${NC}"
+else
+    echo -e "${YELLOW}User 'micrompx' is not a member of the 'audio' group. Adding them to the group now...${NC}"
+    usermod -aG audio micrompx > /dev/null
+fi
 
-echo "Downloading and installing MicroMPX..."
-mkdir -p /opt/micrompx
-wget https://www.stereotool.com/download/MicroMPX_Decoder_ARM64 -O /opt/micrompx/MicroMPX_Decoder
-chmod +x /opt/micrompx/MicroMPX_Decoder
-setcap CAP_NET_BIND_SERVICE=+eip /opt/micrompx/MicroMPX_Decoder
+# Install dependencies for micrompx
+echo -e "${BLUE}►► Installing dependencies...${NC}"
+apt -qq -y install libasound2 > /dev/null 2>&1
 
-echo "Installing MicroMPX service..."
-rm -f /etc/systemd/system/micrompx.service
-wget https://raw.githubusercontent.com/oszuidwest/rpi-umpx-decoder/main/micrompx.service -O /etc/systemd/system/micrompx.service
-systemctl daemon-reload
-systemctl enable micrompx
-service micrompx restart
+# Download micrompx from Thimeo
+echo -e "${BLUE}►► Downloading and installing MicroMPX...${NC}"
+mkdir -p /opt/micrompx > /dev/null
+wget -q https://www.stereotool.com/download/MicroMPX_Decoder_ARM64 -O /opt/micrompx/MicroMPX_Decoder > /dev/null
+chmod +x /opt/micrompx/MicroMPX_Decoder > /dev/null
+setcap CAP_NET_BIND_SERVICE=+eip /opt/micrompx/MicroMPX_Decoder > /dev/null
 
-echo "Disabling onboard audio..."
-CONFIG_FILE="/boot/config.txt"
-# sed -i '/^[^#]*dtparam=audio=on/s/^/#/' "$CONFIG_FILE" # We want to use integrated audio for monitoring the demodulated signal
-sed -i '/dtoverlay=vc4-fkms-v3d/ { /audio=off/! s/$/,audio=off/ }' "$CONFIG_FILE"
-sed -i '/dtoverlay=vc4-kms-v3d/ { /noaudio/! s/$/,noaudio/ }' "$CONFIG_FILE"
+# Add service
+echo -e "${BLUE}►► Installing MicroMPX service...${NC}"
+rm -f /etc/systemd/system/micrompx.service > /dev/null
+wget -q https://raw.githubusercontent.com/oszuidwest/rpi-umpx-decoder/main/micrompx.service -O /etc/systemd/system/micrompx.service > /dev/null
+systemctl daemon-reload > /dev/null
+systemctl enable micrompx > /dev/null
 
-echo "Configuring device tree overlay for HifiBerry..."
-echo "Enter the number corresponding to your device:"
-echo "1. DAC FOR RASPBERRY PI 1/DAC+ LIGHT/DAC ZERO/MINIAMP/BEOCREATE/DAC+ DSP/DAC+ RTC"
-echo "2. DAC+ STANDARD/PRO/XLR"
-echo "3. DAC2 HD"
-echo "4. DAC+ ADC"
-echo "5. DAC+ ADC PRO"
-echo "6. DIGI+"
-echo "7. DIGI+ PRO"
+# Disable only the hdmi audio so we can use the minijack for monitoring
+echo -e "${BLUE}►► Disabling onboard audio...${NC}"
+readonly CONFIG_FILE="/boot/config.txt"
+sed -i '/dtoverlay=vc4-fkms-v3d/ { /audio=off/! s/$/,audio=off/ }' "$CONFIG_FILE" > /dev/null
+sed -i '/dtoverlay=vc4-kms-v3d/ { /noaudio/! s/$/,noaudio/ }' "$CONFIG_FILE" > /dev/null
+
+# Configure the HifiBerry
+echo -e "${BLUE}►► Configuring device tree overlay for HifiBerry...${NC}"
+echo -e "Enter the number corresponding to your device:"
+echo -e "1. DAC FOR RASPBERRY PI 1/DAC+ LIGHT/DAC ZERO/MINIAMP/BEOCREATE/DAC+ DSP/DAC+ RTC"
+echo -e "2. DAC+ STANDARD/PRO/XLR"
+echo -e "3. DAC2 HD"
+echo -e "4. DAC+ ADC"
+echo -e "5. DAC+ ADC PRO"
+echo -e "6. DIGI+"
+echo -e "7. DIGI+ PRO"
 
 read -r device_number
 case $device_number in
@@ -85,20 +120,21 @@ case $device_number in
     5) overlay="hifiberry-dacplusadcpro" ;;
     6) overlay="hifiberry-digi" ;;
     7) overlay="hifiberry-digi-pro" ;;
-    *) echo "Invalid input, exiting."; exit 1 ;;
+    *) echo -e "${RED}Invalid input, exiting.${NC}"; exit 1 ;;
 esac
 
-if ! grep -q "dtoverlay=$overlay" $CONFIG_FILE; then
-    echo "dtoverlay=$overlay" >> $CONFIG_FILE
+if ! grep -q "dtoverlay=$overlay" $CONFIG_FILE > /dev/null; then
+    echo -e "dtoverlay=$overlay" >> $CONFIG_FILE
 fi
 
-echo "Checking Linux version and disabling onboard EEPROM if necessary..."
+# Apply HifiBerry kernel fix is needed
+echo -e "${BLUE}►► Checking Linux version and disabling onboard EEPROM if necessary...${NC}"
 kernel_version=$(uname -r | awk -F. '{print $1 "." $2}')
-
-if [ "$(printf "%s\\n" "5.4" "$kernel_version" | sort -V | head -n1)" = "5.4" ] && [ "$kernel_version" != "5.4" ]; then
-    if ! grep -q 'force_eeprom_read=0' $CONFIG_FILE; then
-        echo "force_eeprom_read=0" >> $CONFIG_FILE
-    fi
+if [ "$(printf '%s\n' "5.4" "$kernel_version" | sort -V | head -n1)" = "5.4" ] && [ "$kernel_version" != "5.4" ]; then
+    grep -q 'force_eeprom_read=0' $CONFIG_FILE || echo -e "force_eeprom_read=0" >> $CONFIG_FILE
 fi
 
-echo "Configuration updated. Please reboot your system for the changes to take effect."
+# Reboot
+echo -e "\n\n${GREEN}✓ Setup is complete! Your Raspberry Pi will reboot in 10 seconds.${NC}"
+sleep 10
+reboot
