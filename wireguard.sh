3f4c02dc161d93cc42f85421a7d0e92d7834d85b
Raymon Mens <r.mens@me.com>
2023-05-23 22:53:41 +0200

    Formatting and styling

--- wireguard.sh
+++ wireguard.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Variables (Replace these with the actual values)
-server_public_ip="<server_ip>"
-server_public_key="<server_public_key>"
-network="172.16.0.0/24"
-raspberry_address="172.16.0.2/24" # Assume .1 is the server.
+SERVER_PUBLIC_IP="<server_ip>"
+SERVER_PUBLIC_KEY="<server_public_key>"
+NETWORK="172.16.0.0/24"
+RASPBERRY_ADDRESS="172.16.0.2/24" # Assume .1 is the server.
 
 # Update and install WireGuard
 echo "Updating system and installing WireGuard..."
@@ -16,7 +16,7 @@ umask 077
 mkdir -p /etc/wireguard
 cd /etc/wireguard || exit
 
-if [ ! -f privatekey ] || [ ! -f publickey ]; then
+if [[ ! -f privatekey ]] || [[ ! -f publickey ]]; then
   echo "Generating new key pair..."
   wg genkey | tee privatekey | wg pubkey | tee publickey
   echo "New key pair has been generated and saved in /etc/wireguard."
@@ -25,19 +25,19 @@ else
 fi
 
 # Read the generated private key
-raspberry_private_key=$(cat privatekey)
+RASPBERRY_PRIVATE_KEY="$(cat privatekey)"
 
 # Create WireGuard configuration file
 echo "Creating WireGuard configuration file..."
 bash -c "cat > /etc/wireguard/wg0.conf << EOL
 [Interface]
-Address = ${raspberry_address}
-PrivateKey = ${raspberry_private_key}
+Address = ${RASPBERRY_ADDRESS}
+PrivateKey = ${RASPBERRY_PRIVATE_KEY}
 
 [Peer]
-PublicKey = ${server_public_key}
-Endpoint = ${server_public_ip}:51820
-AllowedIPs = ${network}
+PublicKey = ${SERVER_PUBLIC_KEY}
+Endpoint = ${SERVER_PUBLIC_IP}:51820
+AllowedIPs = ${NETWORK}
 PersistentKeepalive = 25
 EOL"
 
