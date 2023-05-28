e2cadc2031c7ee22933c309a4f9ae84a09db8cf6
Raymon Mens <r.mens@me.com>
2023-05-28 15:54:03 +0200

    Create monit.sh

---
+++ monit.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Define the process and URLs
+PROCESS="/opt/micrompx/MicroMPX_Decoder"
+GENERAL_HEARTBEAT="https://heartbeat.uptimerobot.com/xxxxx"
+MICROMPX_MONITOR="https://heartbeat.uptimerobot.com/xxxxxxx"  # Update this as per your requirements
+
+# Define the cron jobs
+CRON_JOBS=(
+"* * * * * /usr/bin/curl -s $GENERAL_HEARTBEAT > /dev/null"
+"* * * * * pgrep -f $PROCESS > /dev/null && /usr/bin/curl -s $MICROMPX_MONITOR > /dev/null"
+)
+
+# Check each cron job and add it if it doesn't exist
+for job in "${CRON_JOBS[@]}"; do
+    # Check for existing crontab
+    if crontab -l 2>/dev/null; then
+        # Check if cron job exists
+        crontab -l | grep -q "$job"
+        if [[ $? -ne 0 ]]; then
+            (crontab -l; echo "$job") | crontab -
+        fi
+    else
+        # If no existing crontab, add job
+        echo "$job" | crontab -
+    fi
+done
