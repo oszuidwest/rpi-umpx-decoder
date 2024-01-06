2ea027ee6cfa3111289252206165c9a49c2f5368
Raymon Mens <r.mens@me.com>
2024-01-06 02:30:39 +0100

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -29,10 +29,10 @@ is_this_os_64bit
 check_rpi_model 3
 
 # Ask for input for variables
-ask_user "SERVER_PUBLIC_IP" "127.0.0.1" "Enter the ip-address of the Wireguard server" "str"
+ask_user "SERVER_PUBLIC_IP" "8.8.8.8" "Enter the public ip-address of the Wireguard server" "str"
 ask_user "SERVER_PUBLIC_KEY" "GQ4G7V+uRFRbqzYTgNHLd58o+RNPUW99L7Nc7mTt2Hs=" "Enter the public key of the Wirguard server" "str"
 ask_user "NETWORK" "172.18.1.0/24" "Enter the network range you want to allow to connect" "str"
-ask_user "RASPBERRY_ADDRESS" "172.18.1.2/32" "Enter the private ip-address this device should have" "str"
+ask_user "RASPBERRY_ADDRESS" "172.18.1.2" "Enter the private ip-address this device should have" "str"
 
 # Paths
 WIREGUARD_PATH="/etc/wireguard"
