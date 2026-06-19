3a14a61643d53570f1a2229e0503eed01a405068
Raymon Mens <raymon@zuidwestfm.nl>
2026-06-19 22:24:57 +0200

    feat: apply time sync and journald setters (#19)

--- install.sh
+++ install.sh
@@ -119,8 +119,10 @@ else
   echo -e "${YELLOW}MicroMPX service is not running.${NC}"
 fi
 
-# Timezone configuration
+# Configure host time settings and journald storage limits
 set_timezone Europe/Amsterdam
+set_time_sync
+set_journald_limits
 
 # Update the OS
 if [ "$DO_UPDATES" == "y" ]; then
